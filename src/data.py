eb47abbdef50acad6eee9a9209326d10e16250d2
melifluos <benjamin.chamberlain@gmail.com>
2023-03-10 18:15:59 +0700

    data comments and cleanup

--- src/data.py
+++ src/data.py
@@ -1,14 +1,13 @@
-import copy
+"""
+Read and split ogb and planetoid datasets
+"""
+
 import os
-import pickle
 
 import numpy as np
-import pandas as pd
 import torch
 from ogb.linkproppred import PygLinkPropPredDataset
-import torch_sparse
-from torch_geometric.nn.conv.gcn_conv import gcn_norm
-from torch_geometric.data import Data, InMemoryDataset
+from torch_geometric.data import Data
 from torch_geometric.datasets import Planetoid
 from torch_geometric.transforms import RandomLinkSplit
 from torch_geometric.utils import (add_self_loops, negative_sampling,
