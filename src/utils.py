40a9e533229844198a0a81934ef0de6fde625849
melifluos <benjamin.chamberlain@gmail.com>
2023-03-14 10:39:29 +0700

    fixing utils imports and comments

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
+"""
+utility functions and global variables
+"""
+
 import os
 from distutils.util import strtobool
 
-import numpy as np
-import scipy
 import torch
-from scipy.stats import sem
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 
