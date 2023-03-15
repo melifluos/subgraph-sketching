7cd5f3bc43173cd8e2d6e940d2c2ff31f60bc6ab
melifluos <benjamin.chamberlain@gmail.com>
2023-03-15 14:05:09 +0700

    tests for the sign component

--- test/test_sign.py
+++ test/test_sign.py
@@ -6,8 +6,6 @@ import unittest
 
 import torch
 from torch import tensor
-import networkx as nx
-import numpy as np
 from argparse import Namespace
 from torch_geometric.data import Data
 from torch_geometric.utils.random import barabasi_albert_graph
@@ -22,15 +20,11 @@ class SIGNTests(unittest.TestCase):
     def setUp(self):
         self.n_nodes = 30
         degree = 5
-        p = 0.2
         self.x = torch.rand((self.n_nodes, 2))
-        # self.G = nx.newman_watts_strogatz_graph(n=self.n_nodes, k=degree, p=p)
-        # self.A = nx.adjacency_matrix(self.G)
         self.edge_index = barabasi_albert_graph(self.n_nodes, degree)
         self.edge_weight = torch.ones(self.edge_index.shape[1])
         self.A = ssp.csr_matrix((self.edge_weight, (self.edge_index[0], self.edge_index[1])),
                                 shape=(self.n_nodes, self.n_nodes))
-        # self.edge_index = torch.tensor(np.array(self.G.edges)).T
         self.test_edges = tensor([[0, 1], [1, 2]])
         self.neg_test_edges = tensor([[0, 1], [2, 0]])
 
@@ -49,3 +43,27 @@ class SIGNTests(unittest.TestCase):
         sign_k = 0
         x = elph_dataset.generate_sign_features(data, self.edge_index, self.edge_weight, sign_k)
         self.assertTrue(x.shape == data.x.shape)
+
+    def test_sign_forward(self):
+        hidden_channels = 3
+        K = 2
+        batch_size = 16
+        # sign takes a batch of edges
+        x = torch.rand((batch_size, 2, hidden_channels * (K + 1)))  # the original features + K propagated features
+        xs = torch.split(x, K + 1, dim=-1)
+        self.assertTrue(len(xs) == K + 1)
+        self.assertTrue(xs[0].shape == xs[-1].shape)
+        self.assertTrue(x.shape[0] == xs[0].shape[0])
+        torch.manual_seed(0)
+        sign = SIGN(hidden_channels, hidden_channels, hidden_channels, K, 0.5)
+        out = sign(x)
+        self.assertTrue(out.shape == (batch_size, 2, hidden_channels))
+
+    def test_convs(self):
+        hidden_channels = 5
+        x = torch.rand((self.n_nodes, hidden_channels))
+        torch.manual_seed(0)
+        sign = SIGNEmbedding(hidden_channels, hidden_channels, hidden_channels, 2, 0.5)
+        torch.manual_seed(0)
+        sign_forward = sign(x, self.edge_index, self.n_nodes)
+        self.assertTrue(sign_forward.shape == (self.n_nodes, hidden_channels))
