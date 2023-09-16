a441d390d424b19c2d4e415f8943b81b46881cc7
Chen Shao <chenshao@MacBook-Pro-von-Chen.local>
2023-09-16 14:06:02 +0200

    update load

--- src/data_utils/load.py
+++ src/data_utils/load.py
@@ -1,12 +1,12 @@
 import os, json
 
-def get_text_graph(dataset, use_text=False, use_gpt=False, seed=0):
+def get_text_graph(dataset: str, use_text=False, use_gpt=False, seed=0):
     if dataset == 'cora':
-        from core.data_utils.load_cora import get_raw_text_cora as get_raw_text
+        from load_cora import get_raw_text_cora as get_raw_text
     elif dataset == 'pubmed':
-        from core.data_utils.load_pubmed import get_raw_text_pubmed as get_raw_text
+        from load_pubmed import get_raw_text_pubmed as get_raw_text
     elif dataset == 'ogbn-arxiv':
-        from core.data_utils.load_arxiv import get_raw_text_arxiv as get_raw_text
+        from load_arxiv import get_raw_text_arxiv as get_raw_text
     else:
         exit(f'Error: Dataset {dataset} not supported')
 
@@ -14,23 +14,11 @@ def get_text_graph(dataset, use_text=False, use_gpt=False, seed=0):
     if not use_text:
         data, _ = get_raw_text(use_text=False, seed=seed)
         return data
-
-    # for finetuning LM
-    if use_gpt:
-        data, text = get_raw_text(use_text=False, seed=seed)
-        folder_path = 'gpt_responses/{}'.format(dataset)
-        print(f"using gpt: {folder_path}")
-        n = data.y.shape[0]
-        text = []
-        for i in range(n):
-            filename = str(i) + '.json'
-            file_path = os.path.join(folder_path, filename)
-            with open(file_path, 'r') as file:
-                json_data = json.load(file)
-                content = json_data['choices'][0]['message']['content']
-                text.append(content)
-    else:
+    else:# for finetuning LM
         data, text = get_raw_text(use_text=True, seed=seed)
+        return data, text
 
-    return
-
+if __name__ == '__main__':
+    data, text = get_text_graph(use_text=True)
+    print(data)
+    print(text)
---
+++ src/data_utils/load_cora.py
@@ -0,0 +1,161 @@
+import numpy as np
+import torch
+import random
+
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+
+
+# return cora dataset as pytorch geometric Data object together with 60/20/20 split, and list of cora IDs
+
+
+def get_cora_casestudy(SEED=0):
+    data_X, data_Y, data_citeid, data_edges = parse_cora()
+    # data_X = sklearn.preprocessing.normalize(data_X, norm="l1")
+
+    torch.manual_seed(SEED)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(SEED)
+    np.random.seed(SEED)  # Numpy module.
+    random.seed(SEED)  # Python random module.
+
+    # load data
+    data_name = 'cora'
+    # path = osp.join(osp.dirname(osp.realpath(__file__)), 'dataset')
+    dataset = Planetoid('dataset', data_name,
+                        transform=T.NormalizeFeatures())
+    data = dataset[0]
+
+    data.x = torch.tensor(data_X).float()
+    data.edge_index = torch.tensor(data_edges).long()
+    data.y = torch.tensor(data_Y).long()
+    data.num_nodes = len(data_Y)
+
+    # split data
+    node_id = np.arange(data.num_nodes)
+    np.random.shuffle(node_id)
+
+    data.train_id = np.sort(node_id[:int(data.num_nodes * 0.6)])
+    data.val_id = np.sort(
+        node_id[int(data.num_nodes * 0.6):int(data.num_nodes * 0.8)])
+    data.test_id = np.sort(node_id[int(data.num_nodes * 0.8):])
+
+    data.train_mask = torch.tensor(
+        [x in data.train_id for x in range(data.num_nodes)])
+    data.val_mask = torch.tensor(
+        [x in data.val_id for x in range(data.num_nodes)])
+    data.test_mask = torch.tensor(
+        [x in data.test_id for x in range(data.num_nodes)])
+
+    return data, data_citeid
+
+
+# credit: https://github.com/tkipf/pygcn/issues/27, xuhaiyun
+
+
+def parse_cora():
+    path = 'dataset/cora_orig/cora'
+    idx_features_labels = np.genfromtxt(
+        "{}.content".format(path), dtype=np.dtype(str))
+    data_X = idx_features_labels[:, 1:-1].astype(np.float32)
+    labels = idx_features_labels[:, -1]
+    class_map = {x: i for i, x in enumerate(['Case_Based', 'Genetic_Algorithms', 'Neural_Networks',
+                                             'Probabilistic_Methods', 'Reinforcement_Learning', 'Rule_Learning',
+                                             'Theory'])}
+    data_Y = np.array([class_map[l] for l in labels])
+    data_citeid = idx_features_labels[:, 0]
+    idx = np.array(data_citeid, dtype=np.dtype(str))
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt(
+        "{}.cites".format(path), dtype=np.dtype(str))
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten()))).reshape(
+        edges_unordered.shape)
+    data_edges = np.array(edges[~(edges == None).max(1)], dtype='int')
+    data_edges = np.vstack((data_edges, np.fliplr(data_edges)))
+    return data_X, data_Y, data_citeid, np.unique(data_edges, axis=0).transpose()
+
+
+def get_raw_text_cora_from_rpo(use_text=False, seed=0):
+    """adopted from repo: 
+    https://github.com/XiaoxinHe/TAPE/blob/241c93b735dcebbe2853414395c1559d5c2ce202/core/data_utils/load_cora.py
+    """
+    data, data_citeid = get_cora_casestudy(seed)
+    if not use_text:
+        return data, None
+
+    with open('dataset/cora_orig/mccallum/cora/papers') as f:
+        lines = f.readlines()
+    pid_filename = {}
+    for line in lines:
+        pid = line.split('\t')[0]
+        fn = line.split('\t')[1]
+        pid_filename[pid] = fn
+
+    path = 'dataset/cora_andrew_mccallum/extractions/'
+    text = []
+    for pid in data_citeid:
+        fn = pid_filename[pid]
+        with open(path + fn) as f:
+            lines = f.read().splitlines()
+
+        for line in lines:
+            if 'Title:' in line:
+                ti = line
+            if 'Abstract:' in line:
+                ab = line
+        text.append(ti + '\n' + ab)
+    return data, text
+
+
+def get_raw_text_cora(use_text=False, seed=0):
+    data, data_citeid = get_cora_casestudy(seed)
+    if not use_text:
+        return data, None
+
+    with open('dataset/cora_orig/mccallum/cora/papers') as f:
+        lines = f.readlines()
+    pid_filename = {}
+    for line in lines:
+        pid = line.split('\t')[0]
+        fn = line.split('\t')[1]
+        pid_filename[pid] = fn
+
+    andrew_maccallum_path = 'dataset/cora_andrew_mccallum/extractions/'
+    # path = 'dataset/cora_orig/mccallum/cora/extractions/'
+    text = []
+    whole, founded = len(data_citeid), 0
+    no_ab_or_ti = 0
+    for pid in data_citeid:
+        fn = pid_filename[pid]
+        ti, ab = load_ab_ti(andrew_maccallum_path, fn)
+        founded += 1
+        text.append(ti + '\n' + ab)
+
+        if ti == '' or ab == '':
+            # print(f"no title {ti}, no abstract {ab}")
+            no_ab_or_ti += 1
+    print(f"found {founded}/{whole} papers, {no_ab_or_ti} no ab or ti.")
+    return data, text
+
+
+def load_ab_ti(path, fn):
+    ti, ab = '', ''
+    with open(path + fn) as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        if line.split(':')[0] == 'Title':
+            ti = line
+        elif line.split(':')[0] == 'Abstract':
+            ab = line
+    return ti, ab
+
+
+if __name__ == '__main__':
+    data, data_citeid = get_cora_casestudy()
+    data, text = get_raw_text_cora(use_text=True)
+    print(data)
+    print(data_citeid)
+    print(text)
+
+
+
